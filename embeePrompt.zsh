18006d4611be886e0152c920e4c51bd2bbd70298
Miguel Bermudez <miguel@miguelbermduez.com>
2012-10-15 10:59:19 -0400

    vim and prompt changes

--- embeePrompt.zsh
+++ embeePrompt.zsh
@@ -28,27 +28,26 @@ alias show='cat ~/.dirs'
 # Module: Change directory with bookmarks
 # path: bin/zsh-modules-available/cdbookmarks
 # http://ivan.fomentgroup.org/blog/2010/01/29/zsh-bookmarks-for-cd-change-directory-with-completion/
-
 function cdb_edit() {
   vim ~/.cdbookmarks
 }
-
 function cdb() {
   NewDir=`egrep "^$1	" ~/.cdbookmarks \
      | sed 's/^.*	//'`;
   echo cd $NewDir
   cd $NewDir
 }
-
 function _cdb() {
   reply=(`cat ~/.cdbookmarks | sed 's/	.*$//'`);
 }
-
 compctl -K _cdb cdb
+
+
+# conveince function for gittower
+function gt() {  gittower --status "$*"; }
 
 #Aliases
 alias gs='git status'
 alias clera='clear'
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" 
-
