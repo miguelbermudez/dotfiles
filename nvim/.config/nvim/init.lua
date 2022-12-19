1877b7d7b2d87dfbbab6e8a5f093d256e6ae46a1
Miguel Bermudez <miguel@miguelbermudez.com>
2022-12-19 11:45:17 -0500

    update nvim config for first install

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,9 @@
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.cmd [[packadd packer.nvim]]
+end
+
+
 require('user')
-
-
--- Autocommand that reloads neovim whenever you save the plugins.lua file
--- Credit: https://github.com/MeirKriheli/dotneovim/blob/5728f528f275bd075fe0d246fdfe25d9db1d20b1/lua/user/plugins.lua#L23
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost packer.lua source <afile> | PackerSync
-  augroup end
-]]
-
--- nvim/.config/nvim/lua/user/packer.lua
+++ nvim/.config/nvim/lua/user/packer.lua
@@ -1,3 +1,13 @@
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+-- Credit: https://github.com/MeirKriheli/dotneovim/blob/5728f528f275bd075fe0d246fdfe25d9db1d20b1/lua/user/plugins.lua#L23
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost packer.lua source <afile> | PackerSync
+  augroup end
+]]
+
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
